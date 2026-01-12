baa9ccb99d7d2f3f067f52d7da6d89908cbcb6eb
Eric Oswald <zyon.900@googlemail.com>
2026-01-12 08:20:17 +0100

    Installed TailwindCSS

---
+++ tailwind.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				accent: {
+					DEFAULT: '#10b981', // green-500
+					50: '#ecfdf5',
+					100: '#d1fae5',
+					200: '#a7f3d0',
+					300: '#6ee7b7',
+					400: '#34d399',
+					500: '#10b981',
+					600: '#059669',
+					700: '#047857',
+					800: '#065f46',
+					900: '#064e3b',
+				},
+			},
+		},
+	},
+	plugins: [],
+};
